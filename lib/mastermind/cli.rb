e734d670e23af1801b950c1853b99f300f1292b8
Josh Cheek <josh.cheek@gmail.com>
2014-09-25 09:54:24 -0600

    First commit: spec for cli

---
+++ lib/mastermind/cli.rb
@@ -0,0 +1,15 @@
+class Mastermind
+  class CLI
+    def self.call(interact)
+      # print an intro
+      # print the options
+      # prompt and get your option
+      # loop {
+      #   "p" -> play game
+      #   "i" -> print mastermind instructions
+      #   "q" -> quit
+      #   invalid input -> tell you to try again
+      # }
+    end
+  end
+end
---
+++ spec/cli_spec.rb
@@ -0,0 +1,47 @@
+require 'mastermind/cli'
+
+RSpec.describe Mastermind::CLI do
+  class MockInteract
+  end
+
+  xit 'prints an intro' do
+    interact = MockInteract.new
+    Mastermind::CLI.call interact
+    interact.assert_told_to :print_intro
+  end
+
+  it 'prints the options'
+  it 'prompts and gets input'
+
+  context 'when I choose to play the game' do
+    it 'plays the game'
+    it "doesn't print instructions"
+    it "doesn't quit"
+    it "doesn't print an invalid message"
+    it "prompts again"
+  end
+
+  context "when I choose to see the instructions" do
+    it "prints instructions"
+    it "doesn't play the game"
+    it "doesn't quit"
+    it "doesn't print an invalid message"
+    it "prompts again"
+  end
+
+  context "when I choose to see the instructions" do
+    it "prints a farewell message"
+    it "doesn't play the game"
+    it "doesn't print instructions"
+    it "doesn't print an invalid message"
+    it "doesn't prompt again"
+  end
+
+  context "when my input is invalid" do
+    it "prints an invalid message"
+    it "doesn't print a farewell message"
+    it "doesn't play the game"
+    it "doesn't print instructions"
+    it "prompts again"
+  end
+end
