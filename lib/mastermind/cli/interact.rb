b0a092e74f4fdc68130429ba650da34ffcd03a71
Josh Cheek <josh.cheek@gmail.com>
2014-09-25 10:07:47 -0600

    Interact prints out the intro

--- lib/mastermind/cli/interact.rb
+++ lib/mastermind/cli/interact.rb
@@ -1,8 +1,17 @@
 class Mastermind
   module CLI
     class Interact
-      def print_intro
+      def initialize(stdin, stdout)
+        self.stdin, self.stdout = stdin, stdout
       end
+
+      def print_intro
+        stdout.puts "Welcome to Mastermind!"
+      end
+
+      private
+
+      attr_accessor :stdin, :stdout
     end
   end
 end
--- spec/interact_spec.rb
+++ spec/interact_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 require 'mastermind/cli/interact'
+require 'stringio'
 
 RSpec.describe Mastermind::CLI::Interact do
   it 'implements all the Interact interface methods' do
     MockInteract::INTERFACE_METHODS.each do |interface_method_name|
       expect(Mastermind::CLI::Interact.instance_methods).to include interface_method_name
     end
+  end
+
+  it 'prints the intro' do
+    stdin = StringIO.new
+    stdout = StringIO.new
+    interact = Mastermind::CLI::Interact.new(stdin, stdout)
+    interact.print_intro
+    expect(stdout.string).to_not be_empty
   end
 end
