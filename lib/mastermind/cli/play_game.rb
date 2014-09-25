53bf5d1954bcb18fd113c8c614c21df9ec107fdd
Josh Cheek <josh.cheek@gmail.com>
2014-09-25 11:59:31 -0600

    Add spec for PlayGame

--- lib/mastermind/cli/play_game.rb
+++ lib/mastermind/cli/play_game.rb
@@ -1,7 +1,39 @@
 class Mastermind
   module CLI
-    class PlayGame
-
+    module PlayGame
+      def self.call(interact, secret, valid_colors)
+        # create the game
+        # display the turn
+        # guess = prompt for the guess
+        # loop
+        #   if guess is invalid
+        #     print invalid message
+        #     do not increment turn
+        #   if guess is incorrect
+        #     print stats
+        #     do increment turn
+        #   if guess is correct
+        #     print win message
+        #     break
+      end
     end
   end
 end
+
+# (secret rrgb)
+
+# You are on turn 1
+# Enter a guess ("r", "g", "b", "y")
+# > rrbr
+# Colors in the correct position: 2
+# Colors in the incorrect position: 1
+
+# You are on turn 2
+# Enter a guess ("r", "g", "b", "y")
+# > rzbr
+# Invalid input!
+
+# You are on turn 2
+# Enter a guess ("r", "g", "b", "y")
+# > rrgb
+# Correct you won in 2 turns!
---
+++ spec/play_game_spec.rb
@@ -0,0 +1,76 @@
+require 'mastermind/cli/play_game'
+
+RSpec.describe Mastermind::CLI::PlayGame do
+  def after_running(options)
+    secret       = options.fetch :secret, "r"
+    input        = options.fetch :input,  "r"
+    valid_colors = options.fetch :valid,  ["r", "g", "b", "y"]
+
+    interact = MockInteract.new(input)
+    Mastermind::CLI::PlayGame.call interact, secret, valid_colors
+    interact
+  end
+
+  context 'when there is no input' do
+    def after_running
+      super input: nil, valid: ['r']
+    end
+
+    it 'does not continue prompting for a guess' do
+      after_running.assert_told_to :prompt_guess, 1
+    end
+
+    it 'exits the game with a cant continue message'
+    it 'does not prin last guess stats'
+    it 'does not print invalid input message'
+    it 'does not print the win message'
+  end
+
+  context 'when my guess is invalid' do
+    def after_running
+      super input: "z", valid: ['r']
+    end
+
+    it 'continues prompting for input' do
+      after_running.assert_told_to :prompt_guess, 2
+    end
+
+    it 'does not increment my turn'
+    it 'does not say it cant continue'
+    it 'does not prin last guess stats'
+    it 'does not print invalid input message'
+    it 'does not print the win message'
+  end
+
+  context 'when my guess is incorrect' do
+    def after_running
+      super secret: "g", input: "r", valid: ['r', 'g']
+    end
+
+    it 'continues prompting for input' do
+      after_running.assert_told_to :prompt_guess, 2
+    end
+
+    it 'increments my turn'
+    it 'does not say it cant continue'
+    it 'prints last guess stats'
+    it 'does not print invalid input message'
+    it 'does not print the win message'
+  end
+
+  context 'when my guess is correct' do
+    def after_running
+      super secret: "r", input: "r", valid: ['r', 'g']
+    end
+
+    it 'does not continue prompting for input' do
+      after_running.assert_told_to :prompt_guess, 2
+    end
+
+    it 'does not increment my turn'
+    it 'does not say it cant continue'
+    it 'does not print last guess stats'
+    it 'does not print invalid input message'
+    it 'does not print the win message'
+  end
+end
