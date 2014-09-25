25e7549cb75220d28c65a9b6d285a844c9728bce
Josh Cheek <josh.cheek@gmail.com>
2014-09-25 11:21:10 -0600

    Refactor CLI a bit

--- lib/mastermind/cli.rb
+++ lib/mastermind/cli.rb
@@ -7,25 +7,15 @@ class Mastermind
     def self.call(interact)
       interact.print_intro
       interact.print_options
-
       loop do
         input = interact.prompt_input
-        if input.nil? || input == "q"
-          break
-        elsif input == "i"
-          interact.print_instructions
-        elsif input == "p"
-          PlayGame.call(interact)
-        else
-          interact.print_invalid_message
+        if    input.nil?   then break
+        elsif input == "q" then break
+        elsif input == "i" then interact.print_instructions
+        elsif input == "p" then PlayGame.call(interact)
+        else                    interact.print_invalid_message
         end
       end
-
-      # loop {
-      #   "p" -> play game
-      #   "q" -> quit
-      #   invalid input -> tell you to try again
-      # }
       interact.print_farewell
     end
   end
